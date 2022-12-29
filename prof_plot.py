a08700ee3a4f39fb8a5458f67c09a7b2106f4cef
William Fiore <wllmfiore8@gmail.com>
2022-12-29 14:44:52 -0600

    Improvements to plots and tables, adding P-Pdot diagram

--- prof_plot.py
+++ prof_plot.py
@@ -47,43 +47,43 @@ flux_info = np.loadtxt(f"{DATA_PATH}flux.info",dtype="str")
 for i in range(3):
     if i==0:
         flux57   = [ss[1] for ss in flux_info][:-8][:7]+[ss[1] for ss in flux_info][:-8][8:]
-        flux327  = [ss[2] for ss in flux_info][:-8][:7]+[ss[2] for ss in flux_info][:-8][8:]
-        flux350  = [ss[3] for ss in flux_info][:-8][:7]+[ss[3] for ss in flux_info][:-8][8:]
-        flux430  = [ss[4] for ss in flux_info][:-8][:7]+[ss[4] for ss in flux_info][:-8][8:]
-        flux820  = [ss[5] for ss in flux_info][:-8][:7]+[ss[5] for ss in flux_info][:-8][8:]
-        flux1380 = [ss[6] for ss in flux_info][:-8][:7]+[ss[6] for ss in flux_info][:-8][8:]
-        flux1500 = [ss[7] for ss in flux_info][:-8][:7]+[ss[7] for ss in flux_info][:-8][8:]
-        flux2000 = [ss[8] for ss in flux_info][:-8][:7]+[ss[8] for ss in flux_info][:-8][8:]
+        flux350  = [ss[2] for ss in flux_info][:-8][:7]+[ss[2] for ss in flux_info][:-8][8:]
+        flux430  = [ss[3] for ss in flux_info][:-8][:7]+[ss[3] for ss in flux_info][:-8][8:]
+        flux820  = [ss[4] for ss in flux_info][:-8][:7]+[ss[4] for ss in flux_info][:-8][8:]
+        flux1380 = [ss[5] for ss in flux_info][:-8][:7]+[ss[5] for ss in flux_info][:-8][8:]
+        flux1500 = [ss[6] for ss in flux_info][:-8][:7]+[ss[6] for ss in flux_info][:-8][8:]
+        flux2000 = [ss[7] for ss in flux_info][:-8][:7]+[ss[7] for ss in flux_info][:-8][8:]
         names    = [ss[0] for ss in flux_info][:-8][:7]+[ss[0] for ss in flux_info][:-8][8:]
         fig1 = plt.figure()
         gs1 = gridspec.GridSpec(12, 16)
-    if i==1:
+    elif i==1:
         flux57   = [ss[1] for ss in flux_info][13:]
-        flux327  = [ss[2] for ss in flux_info][13:]
-        flux350  = [ss[3] for ss in flux_info][13:]
-        flux430  = [ss[4] for ss in flux_info][13:]
-        flux820  = [ss[5] for ss in flux_info][13:]
-        flux1380 = [ss[6] for ss in flux_info][13:]
-        flux1500 = [ss[7] for ss in flux_info][13:]
-        flux2000 = [ss[8] for ss in flux_info][13:]
+        flux350  = [ss[2] for ss in flux_info][13:]
+        flux430  = [ss[3] for ss in flux_info][13:]
+        flux820  = [ss[4] for ss in flux_info][13:]
+        flux1380 = [ss[5] for ss in flux_info][13:]
+        flux1500 = [ss[6] for ss in flux_info][13:]
+        flux2000 = [ss[7] for ss in flux_info][13:]
         names = [ss[0] for ss in flux_info][13:]
         fig2 = plt.figure()
         gs2 = gridspec.GridSpec(8, 16)
-    if i==2:
+    elif i==2:
         flux57   = [[ss[1] for ss in flux_info][7]]
-        flux327  = [[ss[2] for ss in flux_info][7]]
-        flux350  = [[ss[3] for ss in flux_info][7]]
-        flux430  = [[ss[4] for ss in flux_info][7]]
-        flux820  = [[ss[5] for ss in flux_info][7]]
-        flux1380 = [[ss[6] for ss in flux_info][7]]
+        flux350  = [[ss[2] for ss in flux_info][7]]
+        flux430  = [[ss[3] for ss in flux_info][7]]
+        flux820  = [[ss[4] for ss in flux_info][7]]
+        flux1380 = [[ss[5] for ss in flux_info][7]]
         names = [[ss[0] for ss in flux_info][7]]
-        fig3 = plt.figure(figsize=(2,6))
+        fig3 = plt.figure(figsize=(1.5,3.5))
+    else:
+        print("This shouldn't happen.")
+        break
         
     periods = []
     dms = []
 
     for n in names:
-        par_path = f"{DATA_PATH}{n}_fiore+22.par"
+        par_path = f"{DATA_PATH}{n}_fiore+23.par"
         with open(par_path, 'r') as infile:
             for l in infile.readlines():
                 if l.startswith("F0"):
@@ -96,15 +96,15 @@ for i in range(3):
                         p0_label = [round(p0_s.value,2),"s"]
                     else:
                         p0_s = 1./f0
-                        p0_ms = int(p0_s.to(u.ms).value)
+                        p0_ms = int(round(p0_s.to(u.ms).value,0))
                         p0_label = [p0_ms,"ms"]
                     periods.append(p0_label)
                 elif l.startswith("DM "):
                     dm = float(l.split()[1])
-                    if dm<10.:
+                    if dm<9.9:
                         dm = round(dm,1)
                     else:
-                        dm = int(dm)
+                        dm = int(round(dm,0))
                     dms.append(dm)
 
     per_u = [f"{pp[0]} {pp[1]}" for pp in periods]
@@ -117,16 +117,15 @@ for i in range(3):
     phase = np.arange(nbin)/float(nbin)
 
     print(names)
-    for nn,pp,dd,s57,s327,s350,s430,s820,s1380,s1500,s2000 in zip(names,per_u,dm_u,flux57,flux327,flux350,flux430,flux820,flux1380,flux1500,flux2000):
+    for nn,pp,dd,s57,s350,s430,s820,s1380,s1500,s2000 in zip(names,per_u,dm_u,flux57,flux350,flux430,flux820,flux1380,flux1500,flux2000):
 
-        profile_fname_57  = f"{DATA_PATH}{nn}_57.15MHz_fiore+22.profile"
-        profile_fname_327 = f"{DATA_PATH}{nn}_327MHz_fiore+22.profile"
-        profile_fname_350 = f"{DATA_PATH}{nn}_350MHz_fiore+22.profile"
-        profile_fname_430 = f"{DATA_PATH}{nn}_430MHz_fiore+22.profile"
-        profile_fname_820 = f"{DATA_PATH}{nn}_820MHz_fiore+22.profile"
-        profile_fname_1380 = f"{DATA_PATH}{nn}_1380MHz_fiore+22.profile"
-        profile_fname_1500 = f"{DATA_PATH}{nn}_1500MHz_fiore+22.profile"
-        profile_fname_2000 = f"{DATA_PATH}{nn}_2000MHz_fiore+22.profile"
+        profile_fname_57  = f"{DATA_PATH}{nn}_57.15MHz_fiore+23.profile"
+        profile_fname_350 = f"{DATA_PATH}{nn}_350MHz_fiore+23.profile"
+        profile_fname_430 = f"{DATA_PATH}{nn}_430MHz_fiore+23.profile"
+        profile_fname_820 = f"{DATA_PATH}{nn}_820MHz_fiore+23.profile"
+        profile_fname_1380 = f"{DATA_PATH}{nn}_1380MHz_fiore+23.profile"
+        profile_fname_1500 = f"{DATA_PATH}{nn}_1500MHz_fiore+23.profile"
+        profile_fname_2000 = f"{DATA_PATH}{nn}_2000MHz_fiore+23.profile"
 
         print(nn)
         row = int(np.floor(count/4))
@@ -148,6 +147,7 @@ for i in range(3):
         showfreq = 0
         freqxshift = -0.05
         freqyshift = 0.175
+        dig = 1 # number of decimal places to list on flux densities
         if nn=="J0141+6303":
             rotate = -7
         elif nn=="J0214+5222":
@@ -170,9 +170,10 @@ for i in range(3):
         elif nn=="J1327+3423":
             showfreq = 1
             shift = 0.0
-            dshift = 0.2
+            dshift = 0.25
             offset = -0.07
             freqyshift = 0.0
+            freqxshift = -0.07
         elif nn=="J1434+7257":
             dshift = 0.3
             offset = -0.03
@@ -184,91 +185,101 @@ for i in range(3):
             freqxshift += 0.35
             freqyshift += -0.07
         elif nn=="J1913+3732":
-            
             freqxshift += -0.02
         elif nn=="J2115+6702":
             freqyshift += 0.2
         elif nn=="J2145+2158":
             freqyshift += 0.1
         elif nn=="J2210+5712":
-            offset = 0.4
-            freqyshift += 0.4
-        elif nn=="J2326+6243":
+            freqxshift -= 0.02
+            freqyshift += 0.2
             rotate = -10
-            freqxshift += 0.55
+        elif nn=="J2326+6243":
+            rotate = -5
+            #freqxshift += 0.55
             freqyshift += 0.1
         elif nn=="J2354-2250":
-            rotate = -20
-            freqxshift += 0.5
+            rotate = -10
+            #freqxshift += 0.5
         else:
             dshift = 0.5
 
         if profile_fname_57 in all_files:
             y = prof_function(profile_fname_57)
             plt.plot(phase,np.roll(y,rotate)+offset,c='black',label="57 MHz" if count == 0 else "")
-            plt.text(0.05,0.0,"57 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7,color='black')
-            plt.text(0.05,-0.05,f"{s57} mJy",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
+            plt.text(freqxshift+0.05,0.0,"57 MHz", \
+                     horizontalalignment='left',verticalalignment='bottom',fontsize=7,color='black')
+            plt.text(freqxshift+0.05,-0.05,f"{int(float(s57))} mJy", \
+                     horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
                      color='black')
-            shift += dshift
-        if profile_fname_327 in all_files:
-            y = prof_function(profile_fname_327)
-            plt.plot(phase,np.roll(y,rotate)+shift+offset,c='purple',label="327 MHz" if count == 0 else "")
-            plt.text(0.05,shift+0.0,"327 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
-                     color='purple')
-            plt.text(0.05,shift-0.05,f"{s327} mJy",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
-                     color='purple')
             shift += dshift
         if profile_fname_350 in all_files:
             y = prof_function(profile_fname_350)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='r',label="350 MHz" if count == 0 else "")
             if nn=="J1327+3423":
                 df = 0.0
-                freqxshift += 0.05
+                #freqxshift += 0.05
             else:
                 df = -1
+            if nn=="J0032+6946" or nn=="J0141+6303" or nn=="J0214+5222" or nn=="J1434+7257" or nn=="J2145+2158":
+                dig = 2
             plt.text(freqxshift+0.05,shift+0.0,"350 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
                      color='r',alpha=showfreq)
-            plt.text(freqxshift+0.05,shift-0.05+freqyshift,f"{s350} mJy",horizontalalignment='left', \
+            plt.text(freqxshift+0.05,shift-0.05+freqyshift,f"{float(s350):.{dig}f} mJy",horizontalalignment='left', \
                      verticalalignment='bottom',fontsize=7+df,color='r')
             shift += dshift
-            if nn=="J1327+3423":
-                freqxshift -= 0.05
+            dig = 1
+#             if nn=="J1327+3423":
+#                 freqxshift -= 0.05
         if profile_fname_430 in all_files:
             y = prof_function(profile_fname_430)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='pink',label="430 MHz" if count == 0 else "")
-            plt.text(0.05,shift+0.0,"430 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
-                     color='pink')
-            plt.text(0.05,shift-0.05,f"{s430} mJy",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
-                     color='pink')
+            plt.text(freqxshift+0.05,shift+0.0,"430 MHz",horizontalalignment='left',verticalalignment='bottom', \
+                     fontsize=7,color='pink')
+            plt.text(freqxshift+0.05,shift-0.05,f"{float(s430):.{dig}f} mJy",horizontalalignment='left', \
+                     verticalalignment='bottom', fontsize=7,color='pink')
             shift += dshift
         if profile_fname_820 in all_files:
             y = prof_function(profile_fname_820)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='b',label="820 MHz" if count == 0 else "")
-            if nn=="J2326+6243":
+            moredecimal = ["J0032+6946","J0141+6303","J0214+5222","J0415+6111","J0957−0619","J1434+7257","J1530−2114", \
+                           "J1816+4510","J1929+6630","J1930+6205","J2104+2830","J2115+6702","J2145+2158","J2210+5712", \
+                           "J2354-2250"]
+            if nn in moredecimal:
+                dig = 2
+            if nn=="J2115+6702":
+                dy = -0.2
+            elif nn=="J2210+5712":
+                dy = -0.2
+            elif nn=="J2326+6243":
                 dy = -0.1
             else:
                 dy = 0.0
             if nn=="J1327+3423":
                 df = 0.0
-                freqxshift += 0.05
+                #freqxshift += 0.05
             else:
                 df = -1
             plt.text(freqxshift+0.05,shift+0.0,"820 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
                      color='b',alpha=showfreq)
-            plt.text(freqxshift+0.05,shift-0.05+freqyshift+dy,f"{s820} mJy",horizontalalignment='left', \
+            plt.text(freqxshift+0.05,shift-0.05+freqyshift+dy,f"{float(s820):.{dig}f} mJy",horizontalalignment='left', \
                      verticalalignment='bottom',fontsize=7+df,color='b')
             shift += dshift
-            if nn=="J1327+3423":
-                freqxshift -= 0.05
+            dig = 1
+#             if nn=="J1327+3423":
+#                 freqxshift -= 0.05
         if profile_fname_1380 in all_files:
+            dig = 2
             y = prof_function(profile_fname_1380)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='cyan',label="1380 MHz" if count == 0 else "")
-            plt.text(0.05,shift+0.0,"1380 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
+            plt.text(freqxshift+0.05,shift+0.0,"1380 MHz",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
                      color='cyan')
-            plt.text(0.05,shift-0.05,f"{s1380} mJy",horizontalalignment='left',verticalalignment='bottom',fontsize=7, \
-                     color='cyan')
+            plt.text(freqxshift+0.05,shift-0.05,f"{float(s1380):.{dig}f} mJy",horizontalalignment='left', \
+                     verticalalignment='bottom', fontsize=7,color='cyan')
             shift += dshift
+            dig = 1
         if profile_fname_1500 in all_files:
+            dig = 2
             y = prof_function(profile_fname_1500)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='cyan',label="1500 MHz" if count == 0 else "")
             if nn=="J1816+4510":
@@ -277,15 +288,18 @@ for i in range(3):
                 dy = 0.05
             else:
                 dy = 0.0
-            plt.text(freqxshift+0.05,shift-0.05+freqyshift+dy,f"{s1500} mJy",horizontalalignment='left', \
+            plt.text(freqxshift+0.05,shift-0.05+freqyshift+dy,f"{float(s1500):.{dig}f} mJy",horizontalalignment='left', \
                      verticalalignment='bottom',fontsize=6,color='cyan')
             shift += dshift
+            dig = 1
         if profile_fname_2000 in all_files:
+            dig = 2
             y = prof_function(profile_fname_2000)
             plt.plot(phase,np.roll(y,rotate)+shift+offset,c='g',label="2000 MHz" if count == 0 else "")
-            plt.text(freqxshift+0.05,shift-0.05+freqyshift,f"{s2000} mJy",horizontalalignment='left', \
+            plt.text(freqxshift+0.05,shift-0.05+freqyshift,f"{float(s2000):.{dig}f} mJy",horizontalalignment='left', \
                      verticalalignment='bottom',fontsize=6,color='g')
             shift += dshift
+            dig = 1
 
         plt.ylim([-0.1,1.25])
         count += 1
@@ -296,21 +310,27 @@ for i in range(3):
     #     if both: plt.text(0.0,0.73,"mJy",horizontalalignment='left',verticalalignment='bottom',fontsize=7,color='b')
 
         if i==0:
-            plt.text(0.08,1.06,f"PSR {nn.replace('-','$-$')}",horizontalalignment='left',verticalalignment='bottom',fontsize=8)
-            plt.text(1.,0.93,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
-            plt.text(0.95,0.78,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            header_y = 1.0
+            plt.text(0.08,header_y+0.06,f"PSR {nn.replace('-','$-$')}", \
+                     horizontalalignment='left',verticalalignment='bottom',fontsize=8)
+            plt.text(1.,header_y-0.07,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            plt.text(0.95,header_y-0.22,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
             ax1.set_xticks([])
             ax1.set_yticks([])
         elif i==1:
-            plt.text(0.05,1.1,f"PSR {nn.replace('-','$-$')}",horizontalalignment='left',verticalalignment='bottom',fontsize=9)
-            plt.text(1.,0.95,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
-            plt.text(0.95,0.8,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            header_y = 1.0
+            plt.text(0.05,header_y+0.1,f"PSR {nn.replace('-','$-$')}", \
+                     horizontalalignment='left',verticalalignment='bottom',fontsize=9)
+            plt.text(1.,header_y-0.05,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            plt.text(0.95,header_y-0.2,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
             ax2.set_xticks([])
             ax2.set_yticks([])
         else:
-            plt.text(0.15,1.2,f"PSR {nn.replace('-','$-$')}",horizontalalignment='left',verticalalignment='bottom',fontsize=9)
-            plt.text(1.,1.15,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
-            plt.text(0.95,1.1,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            header_y = 0.98
+            plt.text(0.1,header_y+0.2,f"PSR {nn.replace('-','$-$')}", \
+                     horizontalalignment='left',verticalalignment='bottom',fontsize=9)
+            plt.text(1.,header_y+0.15,dd,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
+            plt.text(0.95,header_y+0.1,pp,horizontalalignment='right',verticalalignment='bottom',fontsize=6)
             ax3.set_xticks([])
             ax3.set_yticks([])
         
